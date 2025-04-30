fe8f7c8675f3134accc07e61087398efef3c80b9
ssemart <ssemakula169@gmail.com>
2025-04-30 10:16:13 +0300

    Authentication logo2

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,9 +16,9 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
+            <div class="mb-4">
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <x-application-logo class="w-48 h-auto object-contain" />
                 </a>
             </div>
 
